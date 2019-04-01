3f6e34459a480bc85da2eebcaf048f67dd3dd2b0
John Parejko <parejkoj@uw.edu>
2019-04-01 16:32:30 -0700

    Convert all refcats to have fluxes in nJy
    Converted with `meas_algorithms/bin/convert_refcat_to_nJy.py`

--- cfht/ref_cats/sdss-dr9-fink-v5b/config.py
+++ cfht/ref_cats/sdss-dr9-fink-v5b/config.py
@@ -1,10 +1,16 @@
 import lsst.meas.algorithms.ingestIndexReferenceTask
 assert type(config)==lsst.meas.algorithms.ingestIndexReferenceTask.DatasetConfig, 'config is of type %s.%s instead of lsst.meas.algorithms.ingestIndexReferenceTask.DatasetConfig' % (type(config).__module__, type(config).__name__)
+import lsst.meas.algorithms.indexerRegistry
+# Version number of the persisted on-disk storage format.
+# Version 0 had Jy as flux units (default 0 for unversioned catalogs).
+# Version 1 had nJy as flux units.
+# Updated refcat from version 0->1 to have nJy flux units via convert_refcat_to_nJy.py
+config.format_version=1
+
 # String to pass to the butler to retrieve persisted files.
 config.ref_dataset_name='sdss'
 
-# Depth of the HTM tree to make.  Default is depth=7 which gives
-#               ~ 0.3 sq. deg. per trixel.
+# Depth of the HTM tree to make.  Default is depth=7 which gives ~ 0.3 sq. deg. per trixel.
 config.indexer['HTM'].depth=7
 
 config.indexer.name='HTM'
